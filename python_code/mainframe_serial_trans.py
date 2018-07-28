d3e22b5617cee91863d4ea86f47c1301b624bb7b
PeterStine <peter.stine@oit.edu>
2018-07-28 15:17:16 -0700

    Added tensorflow dependencies

--- python_code/mainframe_serial_trans.py
+++ python_code/mainframe_serial_trans.py
@@ -5,8 +5,14 @@
 
 import serial
 
-# For tensorflow to import correctly, the python file must be executed from within the virtual environment
+# For tensorflow to import correctly, the python file must be executed from venv
 import tensorflow as tf
+from tensorflow import keras # tf.keras
+
+# helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+print(tf.__version__)
 
 with serial.Serial('/dev/ttyUSB0', 9600, timeout = 1) as port: # Open Serial port
 	port.write(b'<11,45,0,1>')
