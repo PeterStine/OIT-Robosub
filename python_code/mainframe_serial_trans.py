28ba73b32302fa75e00b1ff9b161a6de51db99ab
PeterStine <peter.stine@oit.edu>
2018-08-01 15:48:15 -0700

    Integrated thruster control with IMU code

--- python_code/mainframe_serial_trans.py
+++ python_code/mainframe_serial_trans.py
@@ -6,17 +6,39 @@
 import serial
 
 # For tensorflow to import correctly, the python file must be executed from venv
-import tensorflow as tf
-from tensorflow import keras # tf.keras
+#wimport tensorflow as tf
+#from tensorflow import keras # tf.keras
 
 # helper libraries
-import numpy as np
-import matplotlib.pyplot as plt
-print(tf.__version__)
+#import numpy as np
+#import matplotlib.pyplot as plt
+#print(tf.__version__)
 
-with serial.Serial('/dev/ttyUSB0', 9600, timeout = 1) as port: # Open Serial port
-	port.write(b'<11,45,0,1>')
-	line = port.readline()  # read '\n' terminated line
+#with serial.Serial('/dev/ttyUSB0', 9600, timeout = 1) as port: # Open Serial port
+	#port.write(b'1600,1600,1600,1600,1600,1600\n')
+	#line = port.readline()  # read '\n' terminated line
 
-print(line)
+port = serial.Serial('/dev/ttyUSB1')
+
+if port.is_open == False:
+	port = serial.Serial('/dev/ttyUSB1') 
+else:
+	print("USB0 and USB1 Failed to open")
+
+print(port.name)
+
+port.baudrate = 115200
+
+if port.is_open:
+	
+	print("Connection opened")
+	start = "Failure"
+	start = port.readline(50)
+	
+	print(start)
+	port.write(b'<1600,1600,1600,1600,1600,1600>')
+
+else:
+	print("Connection failed")
+
 port.close()
--- python_code/tf_test.py
+++ python_code/tf_test.py
@@ -1,3 +1,6 @@
+# To open this file in interactive mode:
+# https://stackoverflow.com/questions/4624416/is-there-a-possibility-to-execute-a-python-script-while-being-in-interactive-mod
+
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
@@ -5,6 +8,7 @@ from tensorflow import keras
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
+%matplotlib inline
 
 print("TensorFlow " + tf.__version__)
 
@@ -13,3 +17,30 @@ fashion_mnist = keras.datasets.fashion_mnist
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print("\nTraining image quantity and dim: ")
+print(train_images.shape)
+print("\nNumber of training labels: ")
+print(len(train_labels))
+print("\ntrain_labels: ")
+print(train_labels)
+print("\nTest image quantity and dim: ")
+print(test_images.shape)
+print("\nNumber of test labels: ")
+print(len(test_labels))
+print("\ntest_labels: ")
+print(test_labels)
+
+# Show the second image in the training set
+plt.figure()
+plt.imshow(train_images[1])
+plt.colorbar()
+plt.gca().grid(False)
+plt.show()
+
+# Scale [0,255] pixel values to floats between [0,1]
+# Cast to float, then simply divide by 255 
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
