88d2957746961307f6f4d457d55e752bb0fb2acf
PeterStine <peter.stine@oit.edu>
2018-11-16 18:55:34 -0800

    Deleted unecessary files, cleaned up main

--- python_code/main.py
+++ python_code/main.py
@@ -17,42 +17,6 @@
 # https://www.arduino.cc/en/Serial/Begin
 
 
-# All of the following functions need to be placed into separate files.
-# ********** Functions **********
-def cast_float(n):
-	try:
-		return float(n)
-	except ValueError:
-		return float(0)
-
-def set_thrusters(a,b,c,d,e,f,delay):
-	print("Use time.time()")
-
-def set_thrusters_DEPRECATED(a,b,c,d,e,f,delay):
-        # Jay Sucharitakul
-        seconds = 11 # Initial time must be the time+1 (now 10+1)
-        timer = None
-
-        seconds -= 1
-        if seconds==0:
-                # Peter's code
-                line = str(1500 + a) + b',' + str(1500 + b) + b',' + str(1500 + c) + b',' + str(1500 + d) + b',' + str(1500 + e) + b',' + str(1500 + f) + b'\n'
-                THRUST.write(line)
-                seconds = 10
-        timer = threading.Timer(1, tick)
-        timer.start()
-
-        
-        
-	# Concatenate values relative to stationary
-	#line = str(1500 + a) + b',' + str(1500 + b) + b',' + str(1500 + c) + b',' + str(1500 + d) + b',' + str(1500 + e) + b',' + str(1500 + f) + b'\n'
-	#THRUST.write(line)
-
-	#time.sleep(delay)
-	
-def tprint(string):
-        print("Unfinished")
-
 # ********** Imports *********
 
 import numpy as np
@@ -96,7 +60,6 @@ while True:
                 break
 
 # ********** Video Processing **********
-
 
 
 # ******** Device and Port Release ********
